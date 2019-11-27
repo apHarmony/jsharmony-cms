2c2923e2a0c047f43ab56dd93fc9b9184f06fe3f
Justin Love <jl@apharmony.com>
2019-11-27 07:13:41 -0600

    branch-summary: hide checkout button on current branch

--- models/Branch_Summary.js
+++ models/Branch_Summary.js
@@ -5,6 +5,9 @@ jsh.App[modelid] = new (function(){
     if (xmodel.get('branch_sts') != 'ACTIVE') {
       jsh.$('.Branch_Summary_buttonPublish').hide();
     }
+    if (xmodel.get('branch_is_checked_out') == 1) {
+      jsh.$('.Branch_Summary_buttonCheckout').hide();
+    }
   }
 
 })();
