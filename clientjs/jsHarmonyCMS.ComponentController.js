e5fa0249e95839e1af7b9e999620a2f8385d17ce
Andrew Palczewski <apj@apharmony.com>
2020-02-20 11:38:22 -0600

    Move test form to component controller

--- clientjs/jsHarmonyCMS.ComponentController.js
+++ clientjs/jsHarmonyCMS.ComponentController.js
@@ -80,4 +80,95 @@ exports = module.exports = function(jsh, cms){
       jobj.html(component_content);
     });
   }
+
+  this.loadTestForm = function(callback){
+    var CUST_FORM_CONTAINER = '.test_cust_form_container';
+
+    if(jsh.XModels['Customer']) return callback();
+
+    $('body').append("<div style='display:none;'>\
+        <div class='test_cust_form_container xdialogbox' style='width:400px;'></div>\
+      </div>");
+    //Define the form in-memory
+    jsh.XPage.LoadVirtualModel($(CUST_FORM_CONTAINER)[0], {
+      "id": "Customer",
+      "layout": "form",
+      "buttons": [{"link": "js:_this.showTestMessage()", "icon": "ok", "actions":"BIU", "text":"Test Message"}],
+      "ejs": "<div class='test_sample_ejs'>Sample EJS for Test model</div>",
+      "css": ".test_sample_ejs { background-color:#f0f0f0; border:1px solid #bbb; padding:4px 20px; margin-top:10px; }",
+      "js": function(){ //This function is virtual and cannot reference any variables outside its scope
+        var _this = this;
+        //var modelid = [current model id];
+        //var xmodel = [current model];
+
+        _this.oninit = function(xmodel){
+          //Custom oninit function
+        }
+
+        _this.onload = function(xmodel){
+          //Custom onload function
+        }
+
+        _this.showTestMessage = function(){
+          XExt.Alert('Test Message');
+        }
+      },
+      "oninit":"_this.oninit(xmodel);",
+      "onload":"_this.onload(xmodel);",
+      "fields": [
+        {"name": "cust_id", "caption":"Customer ID", "type": "int", "actions":"B",
+         "control":"textbox", "controlstyle":"width:80px;", "validate": ["IsNumeric","Required"] },
+         
+        {"name": "cust_name", "caption":"Name", "type": "varchar", "length": 256, "actions":"B",
+         "control":"textbox", "controlstyle":"width:260px;", "validate": ["MaxLength:256","Required"] },
+         
+        {"name": "cust_sts", "caption":"Status", "type": "varchar", "length":32,
+         "control":"dropdown", "validate": ["Required"] },
+
+        {"control":"html","value":"<b>Sample HTML:</b> Content"},
+
+        {"name":"save_button","control":"button","value":"Save"},
+        {"name":"cancel_button","control":"button","value":"Cancel","nl":false},
+      ]
+    }, function(custmodel){
+      if(callback) callback();
+    });
+  }
+
+  this.showTestForm = function(){
+    var CUST_FORM_CONTAINER = '.test_cust_form_container';
+
+    var cust_data = {
+      cust_id:   1,
+      cust_name: 'Test Customer',
+      cust_sts:  'ACTIVE',
+    };
+
+    _this.loadTestForm(function(){
+      //Model loaded
+      //Render data
+      jsh.XModels['Customer'].controller.setLOV('cust_sts', [
+        {code_val: '',       code_txt:'Please select...'},
+        {code_val: 'ACTIVE', code_txt:'Active'},
+        {code_val: 'CLOSED', code_txt:'Closed'},
+      ]);
+      jsh.XModels['Customer'].controller.Render(cust_data);
+      //Open dialog
+      XExt.CustomPrompt(CUST_FORM_CONTAINER, $(CUST_FORM_CONTAINER), function(acceptFunc, cancelFunc){ //onInit
+        //Enable the form (so that navigation events trigger check for updates)
+        jsh.XModels['Customer'].controller.form.Prop.Enabled = true;
+        //Attach save / cancel events to dialog events
+        jsh.$root('.save_button.xelemCustomer').off('click').on('click', acceptFunc);
+        jsh.$root('.cancel_button.xelemCustomer').off('click').on('click', cancelFunc);
+      }, function(success){ //onAccept
+        //Commit customer data to API
+        if(!jsh.XModels['Customer'].controller.Commit(cust_data, 'U')) return;
+        XExt.Alert('Saving...'+JSON.stringify(cust_data), success);
+      }, undefined, function(){ //onClosed
+        //Disable the form (so that navigation events do not trigger check for updates)
+        jsh.XModels['Customer'].controller.form.Prop.Enabled = false;
+      }, { reuse: true });
+    });
+  }
+
 }
