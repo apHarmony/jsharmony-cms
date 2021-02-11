fa1e848c639c48003c893362a925705d2c9bd3ca
Andrew Palczewski <apj@apharmony.com>
2021-02-10 18:24:59 -0600

    JSHCMS-336 Format lastMediaPath same as lastLinkPath

--- clientjs/jsHarmonyCMS.Editor.Picker.js
+++ clientjs/jsHarmonyCMS.Editor.Picker.js
@@ -20,6 +20,7 @@ along with this package.  If not, see <http://www.gnu.org/licenses/>.
 exports = module.exports = function(jsh, cms, editor){
   var _this = this;
   var XExt = jsh.XExt;
+  
   this.lastMediaPath = undefined;
   this.lastLinkPath = undefined;
 
@@ -42,11 +43,11 @@ exports = module.exports = function(jsh, cms, editor){
         if(page_key.toString()==patharr[3]) return { init_page_key: page_key };
       }
     }
-    else if(filePickerType === 'link' && _this.lastLinkPath) {
+    else if((filePickerType === 'link') && _this.lastLinkPath) {
       if(_this.lastLinkPath.init_media_path) return { init_media_path: _this.lastLinkPath.init_media_path };
       else if(_this.lastLinkPath.init_page_path) return { init_page_path: _this.lastLinkPath.init_page_path };
     } 
-    else if (filePickerType === 'media' && _this.lastMediaPath) return { init_media_path: _this.lastMediaPath };
+    else if ((filePickerType === 'media') && _this.lastMediaPath) return { init_media_path: _this.lastMediaPath.init_media_path };
 
     return {};
   }
@@ -59,10 +60,7 @@ exports = module.exports = function(jsh, cms, editor){
 
   this.openMedia = function(cb, value, meta){
     cms.filePickerCallback = cb;
-    var qs = { };
-    var linkurl = _this.getParameters('media', value);
-    if(linkurl.init_media_key) qs.init_media_key = linkurl.init_media_key;
-    else if(linkurl.init_media_path) qs.init_media_path = linkurl.init_media_path;
+    var qs = _this.getParameters('media', value);
     XExt.popupForm('jsHarmonyCMS/Media_Browser', 'update', qs, { width: 1100, height: 600 });
   }
 
@@ -73,7 +71,7 @@ exports = module.exports = function(jsh, cms, editor){
       var jdata = JSON.parse(data);
       if(cms.onFilePickerCallback && (cms.onFilePickerCallback(jdata))){}
       else if(jdata.media_key){
-        _this.lastMediaPath = jdata.media_folder;
+        _this.lastMediaPath = { init_media_path: jdata.media_folder };
         _this.lastLinkPath = { init_media_path: jdata.media_folder };
         cms.filePickerCallback(cms._baseurl+'_funcs/media/'+jdata.media_key+'/?media_file_id='+jdata.media_file_id+'#@JSHCMS', jdata);
       }
