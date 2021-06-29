a5715e69d3093c6012b4a4884c696f3b1b84563f
Andrew Palczewski <apj@apharmony.com>
2021-06-29 12:56:11 -0500

    JSHCMS-311 Modify upgrade script to copy previous deployment_target_params

--- models/sql/scripts/upgrade/1-0-62.pgsql.sql
+++ models/sql/scripts/upgrade/1-0-62.pgsql.sql
@@ -1,3 +1,4 @@
 jsharmony.version_increment('jsHarmonyCMS',1,0,62,0);
 
 alter table {schema}.deployment_target add column deployment_target_template_variables text null;
+update {schema}.deployment_target set deployment_target_template_variables = deployment_target_params;
